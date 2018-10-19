65f353f43bb10a173a331779ba9134177c90b9f6
jhosuel <reyesludwig@gmail.com>
2018-10-18 22:33:58 -0400

    first

---
+++ HelloNode/main.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
